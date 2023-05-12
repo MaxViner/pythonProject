2161df291bd118e4d2addc8a8f25e5e4b0053481
max <kodtya.oreshin@inbox.ru>
2023-05-12 17:20:47 +0300

    ско

--- 2пе.py
+++ 2пе.py
@@ -1,9 +1,9 @@
 import sys
-import matplotlib.pyplot as plt
 import random
+import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QHBoxLayout, QLabel, QLineEdit, QTextEdit
 from mpl_toolkits.mplot3d import Axes3D
-
+import numpy as np
 
 class App(QWidget):
     def __init__(self):
@@ -15,6 +15,7 @@ class App(QWidget):
         self.height = 480
         self.coords_log = QTextEdit()
         self.coords = []
+        self.std_devs = [0, 0, 0]
         self.init_ui()
 
     def init_ui(self):
@@ -49,21 +50,37 @@ class App(QWidget):
         coord_input_layout.addWidget(add_line_button)
 
         self.fig_3d = plt.figure()
-        self.ax = self.fig_3d.add_subplot(111, projection='3d')
+        self.ax = self.fig_3d.add_subplot(121, projection='3d')  # Изменили на 121, чтобы разместить графики рядом
 
         # Настройки 3D графика
         self.ax.set_xlabel('X')
         self.ax.set_ylabel('Y')
         self.ax.set_zlabel('Z')
-        self.ax.set_xlim(0, 1000)
-        self.ax.set_ylim(0, 1000)
-        self.ax.set_zlim(0, 1000)
+        self.ax.set_xlim(-10, 10)
+        self.ax.set_ylim(-10, 10)
+        self.ax.set_zlim(-10, 10)
+
+        layout.addWidget(self.fig_3d.canvas)
+
+        self.ax_2d = self.fig_3d.add_subplot(122)
+        self.ax_2d.set_xlabel('X')
+        self.ax_2d.set_ylabel('Z')
+        self.ax_2d.set_xlim(-10, 10)
+        self.ax_2d.set_ylim(-10, 10)
 
         layout.addWidget(self.fig_3d.canvas)
 
         # Добавляем журнал координат в layout
         layout.addWidget(self.coords_log)
 
+        layout.addWidget(self.fig_3d.canvas)
+
+        # Создаем окно для вывода среднеквадратического отклонения
+        self.std_dev_label = QLabel('СКО: x={:.2f}, y={:.2f}, z={:.2f}'.format(*self.std_devs))
+        layout.addWidget(self.std_dev_label)
+
+        layout.addWidget(self.coords_log)
+        self.show()
         self.show()
 
     def add_line(self):
@@ -80,6 +97,21 @@ class App(QWidget):
         # Добавляем координаты в лог
         log_entry = "Линия {0}: {1} -> {2}\n".format(len(self.coords), start, end)
         self.coords_log.append(log_entry)
+        # Рассчитываем среднеквадратическое отклонение по каждой координате
+        self.update_std_devs(end)
+
+        # Обновляем значение среднеквадратического отклонения в окне
+        self.std_dev_label.setText('STD: x={:.2f}, y={:.2f}, z={:.2f}'.format(*self.std_devs))
+
+    def update_std_devs(self, new_coords):
+        # Получаем массив координат конечных точек
+        coords_array = np.array([end for start, end, color in self.coords])
+
+        # Добавляем новые координаты
+        coords_array = np.vstack((coords_array, new_coords))
+
+        # Вычисляем среднеквадратическое отклонение по каждой координате
+        self.std_devs = np.std(coords_array, axis=0)
 
     def plot_3d_line(self, start, end, line_color):
         xs = [start[0], end[0]]
@@ -90,21 +122,11 @@ class App(QWidget):
         self.fig_3d.canvas.draw()
 
     def draw_2d_line(self, end, index, line_color):
-        fig_2d = plt.figure()
-        ax2 = fig_2d.add_subplot(111)
-
-        # Настройки 2D графика
-        ax2.set_xlabel('X')
-        ax2.set_ylabel('Z')
-        ax2.set_xlim(-1000, 1000)
-        ax2.set_ylim(-1000, 1000)
-
-        # Рисуем каждую конечную координату из списка на 2D графике
         xs = end[0]
         zs = end[2]
-        ax2.scatter(xs, zs, color=line_color)
+        self.ax_2d.scatter(xs, zs, color=line_color, marker='o')  # Рисуем точку на 2D-графике
 
-        plt.show()
+        self.fig_3d.canvas.draw()  # Обновляем графики
 
 
 if __name__ == '__main__':
